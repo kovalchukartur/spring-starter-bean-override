01186413c8e6953e89cffb34b37fdae0cd1d9de0
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-12-08 15:13:14 +0200

    Init

---
+++ src/main/java/ua/kovalchuk/springstarterbean/SpringStarterBeanApplication.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.springstarterbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStarterBeanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kovalchuk/springstarterbean/SpringStarterBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.springstarterbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
